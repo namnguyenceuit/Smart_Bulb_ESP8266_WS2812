7d1a9a8d2f7ef97bd232f06467c974e6c40d647e
namnguyenceuit <ewardsnguyen@gmail.com>
2017-10-14 01:25:34 +0700

    Fixed: show() two times call

---
+++ user/.output/eagle/debug/obj/esp8266_arduino_gpio.d
@@ -0,0 +1,13 @@
+.output/eagle/debug/obj/esp8266_arduino_gpio.o .output/eagle/debug/obj/esp8266_arduino_gpio.d : esp8266_arduino_gpio.c \
+ ../include/esp8266_arduino_gpio.h \
+ /home/bionic/ESP8266_RTOS_SDK/include/espressif/c_types.h \
+ /home/bionic/esp-open-sdk/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stdint.h \
+ /home/bionic/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/stdint.h \
+ /home/bionic/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /home/bionic/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/newlib.h \
+ /home/bionic/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/config.h \
+ /home/bionic/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/ieeefp.h \
+ /home/bionic/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/features.h \
+ /home/bionic/ESP8266_RTOS_SDK/extra_include/xtensa/config/core-isa.h \
+ /home/bionic/esp-open-sdk/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stdbool.h \
+ /home/bionic/esp-open-sdk/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stddef.h
--- user/neorings_light_task.c
+++ user/neorings_light_task.c
@@ -28,15 +28,15 @@ void NeoRings_Task(void *pArg)
 	mailbox = (Task_Mailbox*)pArg;
 
 	//init ws2812
-	WS2812_Init(16, 5, NEO_GRB + NEO_KHZ800);
+	WS2812_Init(12, 4, NEO_GRB + NEO_KHZ800);
 	WS2812_Begin();
 
 	//light up first
-	mailbox->neorings[0] = 20, 	//Brightness
+	mailbox->neorings[0] = 10, 	//Brightness
 	//mailbox->neorings[1] = 0,		//White
 	mailbox->neorings[1] = 179,		//Blue
-	mailbox->neorings[2] = 52,		//Green
-	mailbox->neorings[3] = 255,		//Red
+	mailbox->neorings[2] = 0,		//Green
+	mailbox->neorings[3] = 0,		//Red
 	//mailbox->neorings[5] = 0;		//fix
 
 	//255 52 179
@@ -77,17 +77,18 @@ bool NeoRings_Light_isDiff(Task_Mailbox_NeoRings* light1, Task_Mailbox_NeoRings*
 
 void NeoRings_UpdateLight(Task_Mailbox_NeoRings* light)
 {
+
 	//for loop
 	int i = 0;
 	//analyze light data
 	//light: red green blue white brightness
+	WS2812_SetBrightness( light[0] );
 	for(i=0; i<12; i++)
+	{
 		// Moderately bright green color.
 		WS2812_SetPixelColor( i, light[3], light[2], light[1], 0);
-	WS2812_SetBrightness( light[0] );
-
+		WS2812_Show();
+	}
 	// This sends the updated pixel color to the hardware.
-	// problem for calling WS2812_Show() twice, if call it once, led #1 will light green
-	WS2812_Show();
-	WS2812_Show();
+
 }
