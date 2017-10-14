79ffaaedcddb2aab48cc6ad5fbb5484b91a7ca6a
namnguyenceuit <ewardsnguyen@gmail.com>
2017-10-14 12:42:43 +0700

    Modified: Simplified

--- user/neorings_light_task.c
+++ user/neorings_light_task.c
@@ -8,6 +8,8 @@
 #include "../include/neorings_light_task.h"
 #include "esp8266_arduino_ws2812.h"
 
+
+
 xTaskHandle NeoRings_Init(void *pArg)
 {
 	xTaskHandle neorings;
@@ -17,6 +19,31 @@ xTaskHandle NeoRings_Init(void *pArg)
 	return neorings;
 }
 
+void NeoRings_Task(void *pArg)
+{
+	WS2812_Init(12, 4, NEO_GRB + NEO_KHZ800);
+	WS2812_Begin();
+	int i = 0;
+	int j = 0;
+	Task_Mailbox* mailbox;
+	mailbox = (Task_Mailbox*) pArg;
+	mailbox->neorings[0] = 255, 	//Brightness
+	//mailbox->neorings[1] = 0,		//White
+	mailbox->neorings[1] = 255,		//Blue
+	mailbox->neorings[2] = 255,		//Green
+	mailbox->neorings[3] = 255,		//Red
+		//analyze light data
+		//light: red green blue white brightness
+	WS2812_SetBrightness(mailbox->neorings[0]);
+	for(i=0; i<12; i++)
+	{
+		// Moderately bright green color.
+		WS2812_SetPixelColor(i, mailbox->neorings[3], mailbox->neorings[2], mailbox->neorings[1], 0);
+		//WS2812_SetPixelColor(i, light[3], light[2], light[1], 0);
+		WS2812_Show();
+	}
+}
+/*
 void NeoRings_Task(void *pArg)
 {
 	//task's mailbox
@@ -32,11 +59,11 @@ void NeoRings_Task(void *pArg)
 	WS2812_Begin();
 
 	//light up first
-	mailbox->neorings[0] = 10, 	//Brightness
+	mailbox->neorings[0] = 15, 	//Brightness
 	//mailbox->neorings[1] = 0,		//White
 	mailbox->neorings[1] = 179,		//Blue
-	mailbox->neorings[2] = 0,		//Green
-	mailbox->neorings[3] = 0,		//Red
+	mailbox->neorings[2] = 52,		//Green
+	mailbox->neorings[3] = 255,		//Red
 	//mailbox->neorings[5] = 0;		//fix
 
 	//255 52 179
@@ -73,22 +100,31 @@ bool NeoRings_Light_isDiff(Task_Mailbox_NeoRings* light1, Task_Mailbox_NeoRings*
 			return true;
 
 	return false;
-}
-
+} */
+/*
 void NeoRings_UpdateLight(Task_Mailbox_NeoRings* light)
 {
 
 	//for loop
 	int i = 0;
+	int j = 0;
+	Task_Mailbox* mailbox;
+	mailbox = (Task_Mailbox*) pArg;
+	mailbox->neorings[0] = 255, 	//Brightness
+	//mailbox->neorings[1] = 0,		//White
+	mailbox->neorings[1] = 0,		//Blue
+	mailbox->neorings[2] = 255,		//Green
+	mailbox->neorings[3] = 0,		//Red
 	//analyze light data
 	//light: red green blue white brightness
-	WS2812_SetBrightness( light[0] );
+	WS2812_SetBrightness(mailbox->neorings[0]);
 	for(i=0; i<12; i++)
 	{
 		// Moderately bright green color.
-		WS2812_SetPixelColor( i, light[3], light[2], light[1], 0);
+		WS2812_SetPixelColor(i, mailbox->neorings[3], mailbox->neorings[2], mailbox->neorings[1], 0);
+		//WS2812_SetPixelColor(i, light[3], light[2], light[1], 0);
 		WS2812_Show();
 	}
 	// This sends the updated pixel color to the hardware.
 
-}
+} */
